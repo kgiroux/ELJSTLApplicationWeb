2ccc59a757b6ea65a467b4e5098fdff211655d74
LirycMoaners <cyril.lefebvre.27@gmail.com>
2016-02-01 15:29:39 +0100

    Servlet + Bean

---
+++ src/beans/Data.java
@@ -0,0 +1,31 @@
+package beans;
+
+public class Data {
+	private int ssn;
+	private int multiplier;
+	private int bonus;
+	
+	public int getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(int ssn) {
+		this.ssn = ssn;
+	}
+
+	public int getMultiplier() {
+		return multiplier;
+	}
+
+	public void setMultiplier(int multiplier) {
+		this.multiplier = multiplier;
+	}
+
+	public int getBonus() {
+		return bonus;
+	}
+
+	public void setBonus(int bonus) {
+		this.bonus = bonus;
+	}
+}
--- src/servlet/test_servlet.java
+++ src/servlet/test_servlet.java
@@ -1,11 +1,14 @@
 package servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import beans.Data;
 
 /**
  * Servlet implementation class test_servlet
@@ -13,27 +16,49 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/test_servlet")
 public class test_servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public test_servlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public test_servlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void processRequest(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+
+		Data data = new Data();
+
+		data.setBonus(Integer.parseInt(request.getParameter("SSN")));
+		data.setBonus(Integer.parseInt(request.getParameter("multiplier")));
+		data.setBonus(data.getMultiplier()*10);
+
+		request.setAttribute("data", data);
+
+		request.getRequestDispatcher("index.jsp")
+				.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		
+		processRequest(request,response);
 	}
 
 }
